71471bce0851bbb778d9b82e2652c198928bd3a0
Fred Astaire <lancebc@thegodardgroup.com>
2019-04-25 02:25:41 -0400

    updated html files

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,18 @@
 var friendArray = [
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "The Mask",
+        "photo": "https://ae01.alicdn.com/kf/HTB1IWWZc4k98KJjSZFoq6xS6pXaF/Free-Shipping-Halloween-Carnival-Masquerade-Adult-Cosplay-The-Mask-Costume-Props-Eco-friendly-Latex-Jim-Carrey.jpg_640x640.jpg",
         "scores": [
+            3,
+            4,
+            3,
+            5,
+            2,
             1,
             2,
             3,
             4,
-            5,
-            1,
-            2,
-            3,
-            5,
-            1
+            5
         ]
     },
     {
@@ -47,7 +47,7 @@ var friendArray = [
             3
         ]
     }
- 
+
 ]
 
 // Note how we export the array. This makes it accessible to other files using require.
